7d3854d1014e1afcd722c28150096ec9a7b6e251
Evgeniy <btnt51@yandex.ru>
2025-07-10 18:46:53 +0000

    Рефакторинг

--- src/metric.cpp
+++ src/metric.cpp
@@ -22,16 +22,12 @@
 
 namespace analyser::metric {
 
-void MetricExtractor::RegisterMetric(std::unique_ptr<IMetric> metric) {
-    metrics.push_back(std::move(metric));
-}
+void MetricExtractor::RegisterMetric(std::unique_ptr<IMetric> metric) { metrics.push_back(std::move(metric)); }
 
 MetricResults MetricExtractor::Get(const function::Function &func) const {
     MetricResults result;
     result.reserve(metrics.size());
-    std::ranges::for_each(metrics, [&func, &result](auto&& metric) {
-        result.emplace_back(metric->Calculate(func));
-    });
+    std::ranges::for_each(metrics, [&func, &result](auto &&metric) { result.emplace_back(metric->Calculate(func)); });
     return result;
 }
 
